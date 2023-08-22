824f81ee044a48e051c7b0935598295e00ed711c
Diego <dhiego1510@gmail.com>
2023-08-22 15:26:00 -0500

    Implementanto funcionalidad tarea

--- js/main2.js
+++ js/main2.js
@@ -2,10 +2,19 @@
 //crear objeto dentro de un array para agregar tarea;
 const Formulaio = document.querySelector(".modalCreate");
 
-function agregarTarea(array){
-    Formulaio.addEventListener("submit", (e)=>{
-        console.log(e);
+function agregarTarea(array) {
+    Formulaio.addEventListener("submit", (e) => {
+
+        const tarea = {
+            Nombre: e.target.nombre.value,
+            Categoria: e.target.categoria.value,
+            Prioridad: e.target.prioridad.value,
+            Fecha: e.target.fecha.value
+        }
+        array.push(tarea)
     })
+
+
 }
 
 agregarTarea();

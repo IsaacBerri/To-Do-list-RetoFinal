6121deb498b3c442f335a97191c9e4e1781fe2af
deiberjerez <deiber22cora@gmail.com>
2023-09-04 09:20:30 -0500

    creando la funcion para editar tareas(todavia le falta)

--- js/main3.js
+++ js/main3.js
@@ -1,7 +1,23 @@
 export function eliminarTarea(array, id) {
-  let delateTask = array.filter((tarea) => tarea.Nombre !== id);
+  let delateTask = array.filter(function (tarea) {
+    return tarea.Nombre !== id;
+  });
 
   localStorage.removeItem("List");
   localStorage.setItem("List", JSON.stringify([...delateTask]));
+  location.reload();
+}
+
+export function editarTarea(formulario, array, index) {
+  let newTask = {
+    Nombre: formulario.children[0].value,
+    Prioridad: formulario.children[1].value,
+    Categoria: formulario.children[2].value,
+    Fecha: formulario.children[3].value,
+  };
+  array.splice(index, 1, newTask);
+
+  localStorage.removeItem("List");
+  localStorage.setItem("List", JSON.stringify([...array]));
   location.reload();
 }

5d0eb5182a99418e9f67a1c5ddd696863d7730b1
Diego <dhiego1510@gmail.com>
2023-09-05 15:59:10 -0500

    Finalizando la funcion de filtro

--- js/main2.js
+++ js/main2.js
@@ -1,19 +1,72 @@
 const formulario = document.querySelector(".containerModal");
+const container = document.querySelector(".containerList");
 
 export function agregarTarea(array) {
-  const tarea = {
-    Nombre: formulario.children[1].children[0].value,
-    Prioridad: formulario.children[1].children[1].value,
-    Categoria: formulario.children[1].children[2].value,
-    Fecha: formulario.children[1].children[3].value,
-    Completada: false
-  };
-  window.localStorage.setItem("List", JSON.stringify([...array, tarea]));
-  array.push(tarea);
-  location.reload();
+  if (formulario.children[1].children[0].value !== "") {
+    const tarea = {
+      Nombre: formulario.children[1].children[0].value,
+      Prioridad: formulario.children[1].children[1].value,
+      Categoria: formulario.children[1].children[2].value,
+      Fecha: formulario.children[1].children[3].value,
+      Completada: false,
+    };
+    window.localStorage.setItem("List", JSON.stringify([...array, tarea]));
+    location.reload();
+  }
 }
 
-export function filtrarTareas(){
-  console.log("Puto");
-}
+export function filtrarTareas(tipoFiltro) {
+  const convercionArray = [...container.children];
 
+  function eliminarActive() {
+    convercionArray.map((x) => {
+      x.classList.remove("active");
+      x.classList.add("task");
+    });
+  }
+
+  if (tipoFiltro === "Todo") {
+    eliminarActive();
+  } else if (tipoFiltro === "Prioridad") {
+    eliminarActive();
+    const filtro = convercionArray.filter(
+      (x) => x.children[1].innerText == "Ninguna"
+    );
+    eliminarActive();
+    filtro.map((x) => {
+      x.classList.remove("task");
+      x.classList.add("active");
+      console.log(x);
+    });
+
+  } else if (tipoFiltro === "Fecha") {
+    console.log(tipoFiltro);
+    const filtro = convercionArray.filter((x) => x.children[3].innerText == "");
+    console.log(filtro);
+    eliminarActive();
+    filtro.map((x) => {
+      x.classList.remove("task");
+      x.classList.add("active");
+    });
+
+  } else if (tipoFiltro === "Completada") {
+    const filtro = convercionArray.filter(
+      (x) => x.children[0].className == "tachado"
+    );
+    eliminarActive();
+    filtro.map((x) => {
+      x.classList.remove("task");
+      x.classList.add("active");
+    });
+    
+  } else {
+    const filtro = convercionArray.filter(
+      (x) => x.children[2].innerText !== tipoFiltro
+    );
+    eliminarActive();
+    filtro.map((x) => {
+      x.classList.remove("task");
+      x.classList.add("active");
+    });
+  }
+}
